e6d95350d34879a0a149ce06c14ea435262ea50a
Piyush <Piyush.Dharkar@gmail.com>
2018-05-28 07:57:25 +0530

    Removed unnecessary features.

--- Algo.py
+++ Algo.py
@@ -1,3 +1,10 @@
 import pandas as pd
+import numpy as np
 
 dataset = pd.read_csv("dataset.csv")
+
+removed_features = ['objid', 'run', 'rerun', 'fiberid',
+                    'camcol', 'specobjid', 'plate', 'mjd',
+                    'field']
+
+dataset = dataset.drop(removed_features, axis=1)
